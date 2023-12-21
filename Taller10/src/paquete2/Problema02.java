799722f22a1e3a65c6ebd42bc5b433b34bdd81fe
Luis Cordova <lcordovacarrion@gmail.com>
2023-12-21 16:28:54 -0500

    Taller

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -5,10 +5,35 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        String[] vendedores = {"Jessica Cole", "Robert Wallace"};
+        double total = 0;
+        double[][] ventas = new double[2][5];
+        for (int f = 0; f < ventas.length; f++) {
+            System.out.println("Ingrese las ventas diarias de 5 dias de "
+                    + vendedores[f]);
+            for (int c = 0; c < ventas[f].length; c++) {
+                ventas[f][c] = entrada.nextDouble();
+            }
+        }
+        for (int f = 0; f < ventas.length; f++) {
+            for (int c = 0; c < ventas[f].length; c++) {
+                total = total + ventas[f][c];
+            }
+        }
+        System.out.printf("Vendedor(a) %s\nVendedor(a) %s\nHa realizado "
+                + "un total de %.2f en ventas.\n", vendedores[0], vendedores[1],
+                total);
+    }
 }
--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,41 @@
  */
 package paquete4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double[][] datos = new double[2][2];
+        double dato;
+        int contador = 0;
+        for (int f = 0; f < datos.length; f++) {
+            for (int c = 0; c < datos[f].length; c++) {
+                System.out.println("Ingrese el dato");
+                dato = entrada.nextDouble();
+                if (dato >= 1000 && dato <= 1199) {
+                    datos[f][c] = dato;
+                } else {
+                    datos[f][c] = 10;
+                    contador++;
+                }
+            }
+        }
+        for (int f = 0; f < datos.length; f++) {
+            for (int c = 0; c < datos[f].length; c++) {
+                System.out.printf("El dato en la posicion [%d][%d] es "
+                        + "%.2f\n",
+                        f, c, datos[f][c], contador);
+            }
+        }
+        System.out.printf("Se agregaron %d datos fuera de rango\n",
+                contador);
+    }
 }
